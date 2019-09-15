33c74ffade0adb0a8052ed65564e7bd241210afb
Binboy <binboy@live.com>
2019-09-15 11:13:37 +0800

    update solution of proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,11 +1,20 @@
+use std::iter::once;
 pub fn build_proverb(list: &[&str]) -> String {
     // unimplemented!("build a proverb from this list of items: {:?}", list)
-    let mut res = String::new();
     if list.is_empty() {
-        return res;
+        return String::new();
     }
-    for i in 0..list.len() - 1 {
-        res = res + &format!("For want of a {} the {} was lost.\n", list[i], list[i + 1]);
-    }
-    res + &format!("And all for the want of a {}.", list.first().unwrap())
+    list.windows(2)
+        .map(|slice| {
+            format!(
+                "For want of a {} the {} was lost.\n",
+                slice.first().unwrap(),
+                slice.last().unwrap()
+            )
+        })
+        .chain(once(format!(
+            "And all for the want of a {}.",
+            list.first().unwrap()
+        )))
+        .collect()
 }
