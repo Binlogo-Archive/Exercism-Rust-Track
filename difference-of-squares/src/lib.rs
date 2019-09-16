649866459de7c2c7912461bf45bb8158b5f5cc69
Binboy <binboy@live.com>
2019-09-16 22:57:44 +0800

    complete & pulish solution of difference-of-squares

---
+++ difference-of-squares/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    sum(n).pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).map(|n| n.pow(2)).sum()
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
+
+fn sum(n: u32) -> u32 {
+    n * (n + 1) / 2
+}
