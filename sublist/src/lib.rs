40f7acb239894ec6696d37f87be539313c101a74
Binboy <binboy@live.com>
2020-01-05 22:52:51 +0800

    submit solution of sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,5 +7,27 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+    if _first_list.is_empty() {
+        return Comparison::Sublist;
+    }
+    if _second_list.is_empty() {
+        return Comparison::Superlist;
+    }
+    if _first_list.len() > _second_list.len() {
+        return match sublist(_second_list, _first_list) {
+            Comparison::Sublist => Comparison::Superlist,
+            Comparison::Superlist => Comparison::Sublist,
+            other => other,
+        };
+    }
+    if _second_list
+        .windows(_first_list.len())
+        .any(|w| w == _first_list)
+    {
+        return Comparison::Sublist;
+    }
+    Comparison::Unequal
 }
