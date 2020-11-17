fbab18ed52df00a999d734825af1c00d29cf279b
王兴彬_Binboy <binboy@live.com>
2020-11-17 23:36:36 +0800

    Update soluton of parallel-letter-frequency

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,25 +1,31 @@
-use rayon::prelude::*;
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    input
-        .par_chunks(worker_count)
-        .map(|chunk| _frequency(chunk))
-        .reduce(
-            || HashMap::new(),
-            |mut acc, map| {
-                map.into_iter()
-                    .for_each(|(c, count)| *acc.entry(c).or_default() += count);
-                acc
-            },
-        )
+    let chunks = input.chunks(worker_count);
+    let mut handles = vec![];
+    for chunk in chunks {
+        let chunk = chunk
+            .into_iter()
+            .flat_map(|s| s.chars().filter(|c| c.is_alphabetic()))
+            .flat_map(|c| c.to_lowercase())
+            .collect();
+        handles.push(thread::spawn(move || _frequency(chunk)));
+    }
+
+    let mut result = HashMap::new();
+    for handle in handles {
+        for (c, count) in handle.join().unwrap() {
+            *result.entry(c).or_default() += count;
+        }
+    }
+
+    return result;
 }
 
-fn _frequency(input: &[&str]) -> HashMap<char, usize> {
+fn _frequency(input: String) -> HashMap<char, usize> {
     input
-        .into_iter()
-        .flat_map(|s| s.chars().filter(|c| c.is_alphabetic()))
-        .flat_map(|c| c.to_lowercase())
+        .chars()
         .fold(HashMap::new(), |mut r: HashMap<char, usize>, char| {
             *r.entry(char).or_default() += 1;
             return r;
