9302131ea33194a2f793d408c24a7ec8014750a3
Binlogo <binboy@live.com>
2021-07-08 23:36:11 +0800

    Submit hamming solution

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,5 +1,13 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    if s1.len() != s2.len() {
+        return None;
+    }
+    let distance = s1
+        .chars()
+        .zip(s2.chars())
+        .filter(|(c1, c2)| c1 != c2)
+        .count();
+    return Some(distance);
 }
