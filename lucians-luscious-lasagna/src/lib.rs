b42886ec68b32ba9bf62f119ade4068e348643fb
Binlogo <binboy@live.com>
2021-11-01 23:30:06 +0800

    Add lucians-luscious-lasagna

---
+++ lucians-luscious-lasagna/src/lib.rs
@@ -0,0 +1,19 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn expected_minutes_in_oven() -> i32 {
+    return 40;
+}
+
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    return expected_minutes_in_oven() - actual_minutes_in_oven;
+}
+
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    return number_of_layers * 2;
+}
+
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    return remaining_minutes_in_oven(actual_minutes_in_oven) + preparation_time_in_minutes(number_of_layers);
+}
