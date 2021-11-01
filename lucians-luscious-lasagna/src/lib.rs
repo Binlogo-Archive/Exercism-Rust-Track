ec04bcd6378f6f506b013b19f927718ec5b75c91
Binlogo <binboy@live.com>
2021-11-01 23:48:24 +0800

    Submit lucians-luscious-lasagna

--- lucians-luscious-lasagna/src/lib.rs
+++ lucians-luscious-lasagna/src/lib.rs
@@ -15,5 +15,6 @@ pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    return remaining_minutes_in_oven(actual_minutes_in_oven) + preparation_time_in_minutes(number_of_layers);
+    return expected_minutes_in_oven() - remaining_minutes_in_oven(actual_minutes_in_oven)
+        + preparation_time_in_minutes(number_of_layers);
 }
