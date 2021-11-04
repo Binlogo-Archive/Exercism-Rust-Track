f724e0a18b9898895541f43bf910ad57aabd24ae
Binlogo <binboy@live.com>
2021-11-04 23:52:03 +0800

    Submit magazine-cutout

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -5,5 +5,20 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let words_count_map = magazine.into_iter().fold(HashMap::new(), |mut map, word| {
+        *map.entry(word).or_insert(1) += 1;
+        map
+    });
+    let node_count_map = note.into_iter().fold(HashMap::new(), |mut map, word| {
+        *map.entry(word).or_insert(1) += 1;
+        map
+    });
+    let result = node_count_map.into_iter().all(|(word, count)| {
+        if words_count_map.contains_key(word) {
+            return words_count_map[word] >= count;
+        } else {
+            return false;
+        }
+    });
+    return result;
 }
