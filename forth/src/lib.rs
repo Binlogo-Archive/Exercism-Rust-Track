bd7211c85c26d4a8b3661f28bddefe2c64cba96c
Binlogo <binboy@live.com>
2021-11-09 23:58:41 +0800

    Add forth

---
+++ forth/src/lib.rs
@@ -0,0 +1,26 @@
+pub type Value = i32;
+pub type Result = std::result::Result<(), Error>;
+
+pub struct Forth;
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    DivisionByZero,
+    StackUnderflow,
+    UnknownWord,
+    InvalidWord,
+}
+
+impl Forth {
+    pub fn new() -> Forth {
+        unimplemented!()
+    }
+
+    pub fn stack(&self) -> &[Value] {
+        unimplemented!()
+    }
+
+    pub fn eval(&mut self, input: &str) -> Result {
+        unimplemented!("result of evaluating '{}'", input)
+    }
+}
