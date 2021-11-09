9d54ec5b57a4265d51767a07e365336f8927b4c3
Binlogo <binboy@live.com>
2021-11-09 23:45:59 +0800

    Submit rpn-calculator

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -8,8 +8,35 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stack: Vec<i32> = vec![];
+    for item in inputs {
+        match item {
+            CalculatorInput::Add => {
+                if let (Some(val1), Some(val2)) = (stack.pop(), stack.pop()) {
+                    stack.push(val1 + val2);
+                }
+            }
+            CalculatorInput::Subtract => {
+                if let (Some(val1), Some(val2)) = (stack.pop(), stack.pop()) {
+                    stack.push(val2 - val1);
+                }
+            }
+            CalculatorInput::Multiply => {
+                if let (Some(val1), Some(val2)) = (stack.pop(), stack.pop()) {
+                    stack.push(val1 * val2);
+                }
+            }
+            CalculatorInput::Divide => {
+                if let (Some(val1), Some(val2)) = (stack.pop(), stack.pop()) {
+                    stack.push(val2 / val1);
+                }
+            }
+            CalculatorInput::Value(val) => stack.push(*val),
+        }
+    }
+    if stack.len() == 1 {
+        stack.pop()
+    } else {
+        None
+    }
 }
